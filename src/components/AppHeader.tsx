ebbae81dd5c4007c8c194b0385ceec16c0b26390
Mayank <mayankpatil2605@gmail.com>
2024-11-13 13:35:48 -0800

    testing Vercel

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -4,7 +4,7 @@ const AppHeader = () => {
   return (
     <Heading textAlign="center" mt={4} mb={4} fontFamily="Georgia" fontSize="7xl">
       <Highlight query="CARDS" styles={{ px: '2', py: '1', bg: 'yellow.200', rounded: 'full' }}>
-        FlashCARDS
+        FlashThem
       </Highlight>
     </Heading>
   );
